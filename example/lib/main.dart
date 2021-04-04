4de2db266d7b58cb232967d5131c60bd5e36678c
maysoonabed <maysoon.abdulqader99@gmail.com>
2021-04-05 01:51:33 +0300

    c

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter MapBox AutoComplete',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
       ),
       debugShowCheckedModeBanner: false,
       home: Home(),
@@ -27,33 +27,28 @@ class _HomeState extends State<Home> {
   final _startPointController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter MapBox AutoComplete example"),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: CustomTextField(
-          hintText: "Select starting point",
-          textController: _startPointController,
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => MapBoxAutoCompleteWidget(
-                  apiKey: Tokens.MAPBOX_ACCESS_TOKEN,
-                  hint: "Select starting point",
-                  onSelect: (place) {
-                    _startPointController.text = place.placeName;
-                  },
-                  limit: 10,
-                  country: "NG",
-                ),
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      child: CustomTextField(
+        hintText: "Select starting point",
+        textController: _startPointController,
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MapBoxAutoCompleteWidget(
+                apiKey: Tokens.MAPBOX_ACCESS_TOKEN,
+                hint: "Select starting point",
+                onSelect: (place) {
+                  _startPointController.text = place.placeName;
+                },
+                limit: 10,
+                country: "NG",
               ),
-            );
-          },
-          enabled: true,
-        ),
+            ),
+          );
+        },
+        enabled: true,
       ),
     );
   }
