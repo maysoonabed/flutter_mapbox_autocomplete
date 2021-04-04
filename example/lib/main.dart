8603437f02e5a7d227d377692d01d77fef6e5a3f
maysoonabed <maysoon.abdulqader99@gmail.com>
2021-04-05 01:56:28 +0300

    ي

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,29 +27,26 @@ class _HomeState extends State<Home> {
   final _startPointController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-      child: CustomTextField(
-        hintText: "Select starting point",
-        textController: _startPointController,
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => MapBoxAutoCompleteWidget(
-                apiKey: Tokens.MAPBOX_ACCESS_TOKEN,
-                hint: "Select starting point",
-                onSelect: (place) {
-                  _startPointController.text = place.placeName;
-                },
-                limit: 10,
-                country: "NG",
-              ),
+    return CustomTextField(
+      hintText: "Select starting point",
+      textController: _startPointController,
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MapBoxAutoCompleteWidget(
+              apiKey: Tokens.MAPBOX_ACCESS_TOKEN,
+              hint: "اختر وجهتك",
+              onSelect: (place) {
+                _startPointController.text = place.placeName;
+              },
+              limit: 10,
+              country: "NG",
             ),
-          );
-        },
-        enabled: true,
-      ),
+          ),
+        );
+      },
+      enabled: true,
     );
   }
 }
